26300a52cf27056e55ef7033fc30c1c1a553183d
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2019-09-18 21:44:09 +0800

    Developed some features of task 1 Added Tower classes Added 2 photos for start and end Edited sample.fxml

---
+++ src/main/java/sample/Catapult.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.scene.image.Image;
+
+public class Catapult extends Tower {
+    Catapult(){
+        super("Catapult", new Image("catapult.png"), 0, 15, 15, 1);
+    }
+}
