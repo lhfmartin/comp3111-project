0028b22f2ff94bd6c54ac4a3d9250ec390c8735d
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2019-09-18 21:44:09 +0800

    Developed some features of task 1 Added Tower classes Added 2 photos for start and end Edited sample.fxml

---
+++ src/main/java/sample/BasicTower.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.scene.image.Image;
+
+public class BasicTower extends Tower {
+    BasicTower(){
+        super("Basic Tower", new Image("basicTower.png"), 0, 10, 10, 1);
+    }
+}
---
+++ src/main/java/sample/LaserTower.java
@@ -0,0 +1,9 @@
+package sample;
+
+import javafx.scene.image.Image;
+
+public class LaserTower extends Tower {
+    LaserTower(){
+        super("Laser Tower", new Image("laserTower.png"), 0, 90, 90, 1);
+    }
+}
