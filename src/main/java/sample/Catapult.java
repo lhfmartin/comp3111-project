f5497c745f60133245a0d527ec7ba2b301992250
Martin Leung <hfleungaf@connect.ust.hk>
2019-09-19 12:02:18 +0800

    Modified Tower class structure and minor bug fixes

--- src/main/java/sample/Catapult.java
+++ src/main/java/sample/Catapult.java
@@ -4,6 +4,15 @@ import javafx.scene.image.Image;
 
 public class Catapult extends Tower {
     Catapult(){
-        super("Catapult", new Image("catapult.png"), 0, 15, 15, 1);
+        super("Catapult", new Image("catapult.png"));
+    }
+
+    Catapult(int x, int y){
+        super("Catapult", new Image("catapult.png"), 0, 15, 15, 1, x, y);
+    }
+
+    @Override
+    public boolean upgrade() {
+        return false;
     }
 }
