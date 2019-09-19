a2188ae375d485218543be4227eca321d7e24f56
Martin Leung <hfleungaf@connect.ust.hk>
2019-09-19 14:15:37 +0800

    Fixed constructor problem of towers

--- src/main/java/sample/IceTower.java
+++ src/main/java/sample/IceTower.java
@@ -8,7 +8,7 @@ public class IceTower extends Tower {
     }
 
     IceTower(int x, int y){
-        super("Ice Tower", new Image("iceTower.png"), 0, 5, 5, 0.5, x, y);
+        super("Ice Tower", new Image("iceTower.png"), 0, 5, 5, 2.5, 0.5, x, y);
     }
 
     @Override
