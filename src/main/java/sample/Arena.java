84994787abacd0703ad17319c143e5551267414f
mavishung <tmmhung@connect.ust.hk>
2019-11-08 20:19:26 +0800

    Implement update and attack methods of towers. This is not the final version of the implementation.

--- src/main/java/sample/Arena.java
+++ src/main/java/sample/Arena.java
@@ -15,6 +15,7 @@ import javafx.fxml.FXML;
 import javafx.scene.layout.*;
 import javafx.geometry.Insets;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.scene.text.Text;
 
 import java.util.ArrayList;
@@ -47,7 +48,9 @@ public class Arena {
 
     @FXML
     private Label labelMoney;
-
+    
+    private int frame=0;
+    
     private static final int ARENA_WIDTH = 480;
     private static final int ARENA_HEIGHT = 480;
     public static final int GRID_WIDTH = 40;
@@ -57,8 +60,9 @@ public class Arena {
 
     private Label grids[][] = new Label[MAX_V_NUM_GRID][MAX_H_NUM_GRID]; //the grids on arena
     private int x = -1, y = 0; //where is my monster
-    private double money = 100;
-    private ArrayList<Tower> towers = new ArrayList();
+    private double money = 10000000;
+    private ArrayList<Tower> towers = new ArrayList<Tower>();
+    private ArrayList<Monster> monsters = new ArrayList<Monster>();
 
 
     /**
@@ -90,6 +94,7 @@ public class Arena {
 
                 if (j % 2 == 0 || i == ((j + 1) / 2 % 2) * (MAX_V_NUM_GRID - 1)){
                     newLabel.setBackground(new Background(new BackgroundFill(Color.WHITE, CornerRadii.EMPTY, Insets.EMPTY)));
+                    getMonsterInfo(j, i);
                 } else{
                     newLabel.setBackground(new Background(new BackgroundFill(Color.GREEN, CornerRadii.EMPTY, Insets.EMPTY)));
                     setDragAndDrop(j, i);
@@ -109,15 +114,75 @@ public class Arena {
 
     @FXML
     private void nextFrame() {
-        if (x == -1) {
-            grids[0][0].setText("M");
-            x = 0;
-            return;
+//        if (x == -1) {
+//            grids[0][0].setText("M");
+//            x = 0;
+//            return;
+//        }
+//        if (y == MAX_V_NUM_GRID - 1)
+//            return;
+//        grids[y++][x].setText("");
+//        grids[y][x].setText("M");
+    	for(int i=0;i<monsters.size();i++) {
+        	Monster a = monsters.get(i);
+        	if(!a.IsAlive()&&!Gameover()) {
+        		grids[a.getY()][a.getX()].setGraphic(null);
+        		monsters.remove(a);
+        	}
         }
-        if (y == MAX_V_NUM_GRID - 1)
-            return;
-        grids[y++][x].setText("");
-        grids[y][x].setText("M");
+    	if(!Gameover()){
+    		moveMonster();
+	    	for (int i = 0 ; i < towers.size() ; i ++) { 
+	    		
+	    		if (towers.get(i) instanceof LaserTower) {
+	    			LaserTower lt = (LaserTower)towers.get(i);
+	    			if (lt.attack(monsters, paneArena) && money >= lt.getCost()) {
+	    				money -= lt.getCost();
+	    				updateMoneyLabel();
+	    			}
+	    		}else {
+	    			towers.get(i).attack(monsters, paneArena);
+	    		}
+	    	}
+
+    	}
+    	if(!Gameover()){
+	    	if(frame%2==0) {
+	    		for(int i =0; i<frame/10+1;i++)
+	    			if (monsters.size() <= 3)
+		    			createMonster();
+	    	}
+//	        if(monsters.size()>1) {
+//	        	Monster t = monsters.get(0);
+//	        	System.out.println(t.getX()+ " " + t.getY());
+//	        }
+
+	    	for(int i=0;i<monsters.size();i++) {
+	        	Monster a = monsters.get(i);
+	        	if(a.getX()<12&&a.getY()<12) {
+		        	if(a.IsAlive()) {
+		        		createMonsterIcon(a);
+						grids[a.getY()][a.getX()].setText(String.valueOf(i));
+		        	}
+		        	else {
+		        		createCollisionIcon(a);
+		        	}
+	        	}
+	        }
+//	    	for (int i = 0 ; i < towers.size() ; i ++) { 
+//	    		
+//	    		if (towers.get(i) instanceof LaserTower) {
+//	    			LaserTower lt = (LaserTower)towers.get(i);
+//	    			if (lt.attack(monsters, paneArena) && money >= lt.getCost()) {
+//	    				money -= lt.getCost();
+//	    				updateMoneyLabel();
+//	    			}
+//	    		}else {
+//	    			towers.get(i).attack(monsters, paneArena);
+//	    		}
+//	    	}
+    	}
+        frame ++;
     }
 
     /**
@@ -130,10 +195,13 @@ public class Arena {
         Label source2 = labelIceTower;
         Label source3 = labelCatapult;
         Label source4 = labelLaserTower;
+        
+        
         source1.setOnDragDetected(new DragEventHandler(source1));
         source2.setOnDragDetected(new DragEventHandler(source2));
         source3.setOnDragDetected(new DragEventHandler(source3));
         source4.setOnDragDetected(new DragEventHandler(source4));
+        
 
         target.setOnDragDropped((event) -> {
             System.out.println("xx");
@@ -255,6 +323,27 @@ public class Arena {
             event.consume();
         });
     }
+    
+    private void getMonsterInfo(int x, int y) {
+    	Label target = grids[y][x];
+		//System.out.println("Enter1");
+    	target.setOnMouseEntered(event->{
+    		if(!target.getText().isEmpty()) {
+    			Monster a  = monsters.get(Integer.parseInt(target.getText()));
+    			Text monsterInfo = new Text(a.getInfo());
+                VBox vbox = new VBox(monsterInfo);
+                vbox.setId("monsterInformation");
+                vbox.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+                vbox.setPadding(new Insets(4));
+                paneArena.getChildren().add(vbox);
+                paneArena.layout();
+                vbox.setLayoutX((x + 1) * GRID_WIDTH + vbox.getWidth() >= ARENA_WIDTH ? x * GRID_WIDTH - vbox.getWidth() : (x + 1) * GRID_WIDTH);
+                vbox.setLayoutY(y * GRID_HEIGHT + vbox.getHeight() >= ARENA_HEIGHT ? (y + 1) * GRID_WIDTH - vbox.getHeight() : y * GRID_HEIGHT);    		}
+    		});
+        target.setOnMouseExited((event1) -> {
+            paneArena.getChildren().remove(paneArena.lookup("#monsterInformation"));
+        });
+    }
 
     private void updateMoneyLabel(){
         labelMoney.setText("Money: " + money);
@@ -288,6 +377,131 @@ public class Arena {
             default:
                 return null;
         }
+    }
+    
+    
+    public void createMonster() {
+    	
+    	Monster monster =null;
+    	switch((int)(Math.random()*3)) {
+    	case 0:	
+    		monster = new Fox(8+frame/10, 2);
+    		break;
+    	case 1:	
+    		monster = new Penguin(10+frame/10, 1);
+    		break;
+    	case 2:	
+    		monster = new Unicorn(12+frame/10, 1);
+    		break;
+       	default:
+    		return;
+    	}
+    	monsters.add(monster);
+    }
+    
+    public void moveMonster(){
+    	for(int i=0;i<monsters.size();i++) {
+        	Monster a = monsters.get(i);
+        	grids[a.getY()][a.getX()].setGraphic(null);
+    		grids[a.getY()][a.getX()].setText("");
+        	if (a instanceof Penguin) {
+        		((Penguin) a).regenHP();
+        	}
+        	if(a.IsAlive()) {
+        		int speed = a.getSpeed();
+        		while(speed>0) {
+        			speed -= 1;
+        			int temp;
+        			if(a.getX()%2==1) {
+        				temp = a.getX()+1;
+        				if(temp<12) {
+        					a.setX(temp);	
+        				}
+        				else {
+        					a.setX(11);	
+        				}
+        			}
+        			else if((a.getX())%4==0) {
+        				if(a.getY()== MAX_V_NUM_GRID-1) {
+            				temp = a.getX()+1;
+            				if(temp<12) {
+            					a.setX(temp);	
+            				}
+            				else {
+            					a.setX(11);	
+            				}
+        				}
+        				else {
+            				temp = a.getY()+1;
+            				if(temp<12) {
+            					a.setY(temp);	
+            				}
+            				else {
+            					a.setX(11);	
+            				}
+        				}
+        			}
+        			else {
+        				if(a.getY()==0) {
+            				temp = a.getX()+1;
+            				if(temp<12) {
+            					a.setX(temp);	
+            				}
+            				else {
+            					a.setX(11);	
+            				}
+        				}
+        				else {
+            				temp = a.getY()-1;
+            				if(temp<12) {
+            					a.setY(temp);	
+            				}
+            				else {
+            					a.setX(11);	
+            				}
+        				}
+        			}
+        		}
+        	}
+        	else {
+        		
+        	}
+    	}
+    }
+    
+    public void createMonsterIcon(Monster a ) {
+        ImageView imageView = new ImageView(a.getImage());
+        imageView.setFitWidth(GRID_WIDTH * 0.9);
+        imageView.setFitHeight(GRID_HEIGHT * 0.9);
+        grids[a.getY()][a.getX()].setGraphic(imageView);
+    }
+    
+    public void createCollisionIcon(Monster a ) {
+        ImageView imageView = new ImageView("collision.png");
+        imageView.setFitWidth(GRID_WIDTH * 0.9);
+        imageView.setFitHeight(GRID_HEIGHT * 0.9);
+        grids[a.getY()][a.getX()].setGraphic(imageView);
+    }
+    
+    public boolean Gameover() {
+    	for(int i=0;i<monsters.size();i++) {
+        	Monster a = monsters.get(i);
+        	if(a.IsAlive()&& a.getX()>= 11 && a.getY()>=0) {
+        		Text GameoverInfo = new Text("Gamover");
+        		VBox vbox = new VBox(GameoverInfo);
+        		vbox.setId("GameoverInformation");
+        		vbox.setBackground(new Background(new BackgroundFill(Color.RED, CornerRadii.EMPTY, Insets.EMPTY)));
+        		vbox.setPadding(new Insets(7));
+        		paneArena.getChildren().add(vbox);
+        		paneArena.layout();
+        		vbox.setLayoutX(200);
+        		vbox.setLayoutY(200);
+        		System.out.println("Gameover");
+        		return true;
+        		//System.exit(0);
+        	}
+    	}
+    	return false;
     }
 
     class DragEventHandler implements EventHandler<MouseEvent> {
--- src/main/java/sample/LaserTower.java
+++ src/main/java/sample/LaserTower.java
@@ -1,23 +1,210 @@
 package sample;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+import javafx.scene.shape.*;
+import javafx.geometry.Point2D;
+
+
 import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
 
 public class LaserTower extends Tower {
+	private Line line;
+	private Line line2;
+	private double required_resources = 10;
+	
     LaserTower(){
         super("Laser Tower", new Image("laserTower.png"));
     }
 
     LaserTower(int x, int y){
-        super("Laser Tower", new Image("laserTower.png"), 0, 90, 90, 45, 1, x, y);
+        super("Laser Tower", new Image("laserTower.png"), 0, 65, 25, 50, 3, x, y);
     }
 
     @Override
-    public boolean upgrade() {
-        return false;
+    public void upgrade() {
+    	setAttack(getAttack()+1);    
+    }
+    
+    public double getCost() {
+    	return required_resources;
+    }
+    
+    public ArrayList<Double> equationofline (ArrayList<Integer> pta, ArrayList<Integer> ptb){
+    	ArrayList<Double> line = new ArrayList<Double>();
+    	double x1 = pta.get(0);
+    	double y1 = pta.get(1);
+    	double x2 = ptb.get(0);
+    	double y2 = ptb.get(1);
+    	double slope = (y1-y2)/(x1-x2);
+    	double c = y1 - slope*x1;
+    	line.add(slope);
+    	line.add(c);
+    	return line;
+    }
+    
+        
+    public Line nearestborder (Double slope, Double c, ArrayList<Integer> monster, ArrayList<Integer> tower){
+    	ArrayList<Point2D> border = new ArrayList<Point2D>();
+    	Line line = new Line();
+    	double max_x = 480;
+    	double max_y = 480;
+    	double min_x = 0;
+    	double min_y = 0;
+
+    	border.add(new Point2D(max_x, slope*max_x+c));
+    	border.add(new Point2D(min_x, slope*min_x+c));
+    	border.add(new Point2D(((max_y-c)/slope), max_y));
+    	border.add(new Point2D(((min_y-c)/slope), min_y));
+    	
+    	ArrayList<Point2D> removed_negative = new ArrayList<Point2D>();
+    	System.out.println("Before remove: " + border);
+    	for (int i = 0 ; i < border.size() ; i++) {
+    		if (border.get(i).getX() >= 0 && border.get(i).getY() >= 0 && border.get(i).getX() <= 480 && border.get(i).getY() <= 480)
+    			removed_negative.add(border.get(i));
+    	}
+    	
+    	border = removed_negative;
+    	if (border.isEmpty()) {
+    		border.add(new Point2D(monster.get(0), max_y));
+    		border.add(new Point2D(monster.get(0), min_y));
+    	}
+    	
+    	System.out.println(border);
+    	for (int i = 0 ; i < border.size() ; i ++) {
+    		Line temp = new Line();
+    		temp.setStartX(monster.get(0));
+    		temp.setStartY(monster.get(1));
+    		temp.setEndX(border.get(i).getX());
+    		temp.setEndY(border.get(i).getY());
+    		temp.setStyle("-fx-stroke: red;");
+    		if (!temp.contains(tower.get(0), tower.get(1))){
+    			line = temp;
+    			break;
+    		}
+
+    	}
+    	return line;
     }
     
     @Override
-    public void attack() {
+    public boolean attack(ArrayList<Monster> monsters, AnchorPane paneArena){
     	
+    	if (line != null && line2 != null) {
+    		paneArena.getChildren().remove(line);
+    		paneArena.getChildren().remove(line2);
+    	}
+    	
+    	ArrayList<Double> distance = new ArrayList<Double>();
+    	ArrayList<Integer> tower_pixel = getpixel(getX(), getY());
+    	System.out.println("x: " + tower_pixel.get(0));
+    	System.out.println("y: " + tower_pixel.get(1));
+    	for (int i = 0 ; i < monsters.size(); i ++) {
+    		ArrayList<Integer> monster_pixel = getpixel(monsters.get(i).getX(), monsters.get(i).getY());
+     		double a = Math.pow((monster_pixel.get(0)-tower_pixel.get(0)),2);
+    		double b = Math.pow((monster_pixel.get(1)-tower_pixel.get(1)),2);
+    		double temp_distance = Math.sqrt(a+b);
+    		distance.add(temp_distance);
+    	}
+    	
+    	
+    	ArrayList<Integer> index = new ArrayList<Integer>();
+    	for (int i = 0 ; i < distance.size(); i++) {
+    		if (distance.get(i) >= 0) {
+    			if (distance.get(i) <= 65){
+					index.add(i);
+					System.out.println(monsters.get(i).getName() + " within the range [0,65]");
+    			}
+    		}
+
+    	}
+    	
+    	HashMap<Integer, Double> ending = new HashMap<Integer, Double>();
+    	for (int i = 0 ; i < index.size(); i ++) {
+    		ArrayList<Integer> monster_pixel = getpixel(monsters.get(index.get(i)).getX(), monsters.get(index.get(i)).getY());
+    		double a = Math.pow((monster_pixel.get(0)-440),2);
+    		double b = Math.pow((monster_pixel.get(1)-0),2);
+    		double temp_distance = Math.sqrt(a+b);
+    		ending.put(index.get(i), temp_distance);		
+    	}
+    	
+    	Entry<Integer, Double> min = null;
+    	for (Entry<Integer, Double> entry : ending.entrySet()) {
+    	    if (min == null || min.getValue() > entry.getValue()) {
+    	        min = entry;
+    	    }
+    	}
+    	
+    	if (min == null) return false;
+
+    	int min_index = min.getKey(); 
+    	
+    	ArrayList<Integer> monster = getpixel(monsters.get(min_index).getX(), monsters.get(min_index).getY());
+    	ArrayList<Integer> tower = getpixel(getX(), getY());
+    	ArrayList<Double> lineequation = equationofline(monster, tower);
+    	
+    	double  slope = lineequation.get(0);
+    	double c = lineequation.get(1);
+    	Line border = nearestborder(slope, c, monster, tower);
+		Line new_line = new Line();
+		
+		new_line.setStartX(monster.get(0));
+		new_line.setStartY(monster.get(1));
+		new_line.setEndX(tower.get(0));
+		new_line.setEndY(tower.get(1));
+		new_line.setStyle("-fx-stroke: red;");
+		Line fullline = new Line();
+		fullline.setStartX(border.endXProperty().doubleValue());
+		fullline.setStartY(border.endYProperty().doubleValue());
+		fullline.setEndX(tower.get(0));
+		fullline.setEndY(tower.get(1));
+		fullline.setStyle("-fx-stroke: blue;");
+		fullline.setStrokeWidth(6);
+
+    	this.line = new_line;
+    	this.line2 = border;
+    	
+    	paneArena.getChildren().addAll(line);
+    	paneArena.getChildren().addAll(line2);
+
+    	
+		ArrayList<Monster> hurt_mon = new ArrayList<Monster>();
+		Point2D t = new Point2D(tower.get(0),tower.get(1) );
+		Point2D edge = new Point2D(border.endXProperty().doubleValue(),border.endYProperty().doubleValue());
+
+		for (int i = 0 ; i < monsters.size() ; i++) {
+			ArrayList<Integer> p = getpixel(monsters.get(i).getX(), monsters.get(i).getY());
+			Point2D cur = new Point2D(p.get(0),p.get(1));
+			if (fullline.contains(p.get(0), p.get(1))) {
+				System.out.println(monsters.get(i).getName() + " is attacked by laser");
+				if (!hurt_mon.contains(monsters.get(i))) 
+					hurt_mon.add(monsters.get(i));
+			}
+			if (!hurt_mon.contains(monsters.get(i)) && t.distance(cur) <= 3) 
+				hurt_mon.add(monsters.get(i)); 
+			
+			if (!hurt_mon.contains(monsters.get(i)) && edge.distance(cur) <= 3) 
+				hurt_mon.add(monsters.get(i)); 
+			
+		}
+
+		for (int i = 0 ; i < hurt_mon.size() ; i ++) {
+			hurt_mon.get(i).setHP((int)Math.round(Math.max(0, hurt_mon.get(i).getHP()-getAttack())));
+		}
+		return true;
+    	
+    }
+    
+    @Override
+    public String getInfo(){
+        return "Name: " + getName() + "\n"
+                + "Min Range: " + getMinRange() + "\n"
+                + "Max Range: " + getMaxRange() + "\n"
+                + "Attack: " + getAttack() + "\n"
+                + "Cost: " + getBuildCost() + "\n"
+                + "Money required for laser shoot: " + required_resources;
     }
 }
