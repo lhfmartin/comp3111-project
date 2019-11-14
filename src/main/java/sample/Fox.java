55b4a1a243a8b1e20ae41ebe30c4d2c18d557189
sharpsonmong <34719299+sharpsonmong@users.noreply.github.com>
2019-11-14 15:11:48 +0800

    Add files via upload
    add monster class

---
+++ src/main/java/sample/Fox.java
@@ -0,0 +1,17 @@
+package sample;
+
+import javafx.scene.image.Image;
+
+public class Fox extends Monster{
+
+    Fox(){
+        super("Fox", new Image("fox.png"));
+    }
+	
+    Fox(int HP, int speed){
+    	super("Fox", new Image("fox.png"), 0, 0, HP, speed);
+    }
+
+
+
+}
---
+++ src/main/java/sample/Penguin.java
@@ -0,0 +1,22 @@
+package sample;
+
+import javafx.scene.image.Image;
+
+public class Penguin extends Monster{
+
+	Penguin(){
+        super("Penguin", new Image("penguin.png"));
+    }
+	
+	Penguin(int HP, int speed){
+    	super("Penguin", new Image("penguin.png"), 0, 0, HP, speed);
+    }
+	public void regenHP() {
+		if(this.getHP()+2 < this.getMaxHP()) {
+			this.setHP(getHP()+2);
+		}
+		else {
+			this.setHP(getMaxHP());
+		}
+	}
+}
