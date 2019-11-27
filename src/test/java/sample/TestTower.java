2d1f2c230f05f50f29e09d5df0baa09875ee54a1
Martin Leung <hfleungaf@connect.ust.hk>
2019-11-27 22:14:54 +0800

    Added ArenaTest

--- src/test/java/sample/TestTower.java
+++ src/test/java/sample/TestTower.java
@@ -1,12 +1,12 @@
-package sample;
-
-import org.junit.Assert;
-import org.junit.Test;
-
-public class TestTower {
-	@Test
-	public void test() {
-		Assert.assertEquals("Kevin", "Kevin");
-	}
-
-}
+//package sample;
+//
+//import org.junit.Assert;
+//import org.junit.Test;
+//
+//public class TestTower {
+//	@Test
+//	public void test() {
+//		Assert.assertEquals("Kevin", "Kevin");
+//	}
+//
+//}
