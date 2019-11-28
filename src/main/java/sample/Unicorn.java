364245d0e3ebad4d453997e990aa5c6ada433722
sharpsonmong <monghoyam@gmail.com>
2019-11-29 02:59:09 +0800

    Add javadoc for monster and arena

--- src/main/java/sample/Unicorn.java
+++ src/main/java/sample/Unicorn.java
@@ -1,13 +1,23 @@
 package sample;
 
 import javafx.scene.image.Image;
-
+/**
+ * This is the Unicorn Class
+ * It has more HP than other monsters. 
+ * @author hoyammong
+ *
+ */
 public class Unicorn extends Monster{
 
 //    Unicorn(){
 //        super("Unicorn", new Image("unicorn.png"));
 //    }
 	
+	/**
+	 * This is the conversion constructor. HP and speed can be set in this constructor
+	 * @param HP HP of the Unicorn
+	 * @param speed speed of the Unicorn
+	 */
     Unicorn(int HP, int speed){
     	super("Unicorn", new Image("unicorn.png"), 0, 0, HP, speed);
     }
