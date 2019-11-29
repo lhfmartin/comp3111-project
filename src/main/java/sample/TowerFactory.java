3e80c7ece20055756e70ba3e3733deef594249ce
Martin Leung <44720140+lhfmartin@users.noreply.github.com>
2019-11-29 15:00:32 +0800

    Added JavaDoc for some Arena methods and TowerFactory

--- src/main/java/sample/TowerFactory.java
+++ src/main/java/sample/TowerFactory.java
@@ -1,6 +1,15 @@
 package sample;
 
+/**
+ * This is the factory class for generating towers of different types
+ * @author lhfmartin
+ */
 public class TowerFactory {
+    /**
+     * Create a new tower of the specified type for the user to drag and drop to the arena.
+     * @param label Name of the tower to be generated
+     * @return A tower object of the specified type
+     */
     public Tower createTower(String label){
         switch (label){
             case "Basic Tower":
@@ -16,6 +25,13 @@ public class TowerFactory {
         }
     }
 
+    /**
+     * Create a new tower of the specified type that will be placed on (x, y) in the arena
+     * @param label Name of the tower to be generated
+     * @param x x-coordinate of the tower
+     * @param y y-coordinate of the tower
+     * @return A tower object of the specified type
+     */
     public Tower createTower(String label, int x, int y){
         switch (label){
             case "Basic Tower":
