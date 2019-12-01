43ed717a7d7945932f46cff131acf2ac48189cec
mavishung <tmmhung@connect.ust.hk>
2019-12-01 17:52:25 +0800

    add JavaDoc for Main class

--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,9 +5,22 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
+/***
+ * This is the Main Class that starts the application
+ * @author tszmoonhung
+ *
+ */
 public class Main extends Application {
-
+	/**
+	 * Default constructor of Main
+	 */
+	public Main() {
+		
+	}
+	
+	/***
+	 * This is the main entry point of the program
+	 */
     @Override
     public void start(Stage primaryStage) throws Exception{
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/sample.fxml"));
@@ -19,7 +32,10 @@ public class Main extends Application {
         appController.createArena();
     }
 
-
+    /**
+     * If the JavaFx works properly,this method will be ignored.
+     * @param args command line arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
